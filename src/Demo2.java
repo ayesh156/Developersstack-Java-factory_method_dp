3925235a2a4438719ab97c984bd48c024a771081
= <=>
2024-07-08 22:54:07 +0530

    Initial Commit

---
+++ src/Demo2.java
@@ -0,0 +1,12 @@
+import resource.Bus;
+import resource.Car;
+import resource.Van;
+import resource.Vehicle;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Vehicle car = Factory.getInstance().getVehicle(VehicleType.CAR);
+        Vehicle van = Factory.getInstance().getVehicle(VehicleType.VAN);
+        Vehicle bus = Factory.getInstance().getVehicle(VehicleType.VAN);
+    }
+}
---
+++ src/Factory.java
@@ -0,0 +1,28 @@
+import resource.Bus;
+import resource.Car;
+import resource.Van;
+import resource.Vehicle;
+
+public class Factory {
+    private static Factory factory;
+
+    public static Factory getInstance(){
+        if (factory==null){
+            factory = new Factory();
+        }
+        return factory;
+    }
+    public  Vehicle getVehicle(VehicleType vehicleType) {
+        switch (vehicleType){
+            case CAR:
+                return new Car();
+            case VAN:
+                return new Van();
+            case BUS:
+                return new Bus();
+            default:
+                return null;
+        }
+
+    }
+}
